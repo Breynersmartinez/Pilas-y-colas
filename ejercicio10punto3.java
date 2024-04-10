a498bf030494797cf1bc82cd30b88a0e4d73ca15
Breynersmartinez <153401516+Breynersmartinez@users.noreply.github.com>
2024-04-10 14:40:16 -0500

    Update ejercicio10punto3.java

--- ejercicio10punto3.java
+++ ejercicio10punto3.java
@@ -1,6 +1,10 @@
 public class pilascolas{
     public static void main(String [] args)throws Exception{
-
-        System.out.println("  ");
+Stack<String> caje = new Stack<>();
+        caje.push(" client");
+        caje.push(" client");
+        caje.push(" client");
+        caje.push(" client");
+        System.out.println(caje.pop();
     }
 }
